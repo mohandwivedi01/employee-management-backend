555a7f877e68eed8501ff70a6b5e51eaf248fb02
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-03-02 10:41:31 +0530

    initial commit

---
+++ src/main/java/com/employee_management/demoemployee_management/DemoemployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.employee_management.demoemployee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoemployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoemployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee_management/demoemployee_management/DemoemployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_management.demoemployee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoemployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
